a20b5086ab73bb43fb685c8383a9df373a22dce8
Leemalgum <withxia1215@naver.com>
2024-04-28 23:20:46 +0900

    refactor: image 경로 수정

--- front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/components/cart/SalesList.vue
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/components/cart/SalesList.vue
@@ -18,12 +18,13 @@
                         <tr v-for="(cart, i) in cart" :key="i" style="text-align: center;">
                             <td class="product-infomation" style="text-align: left;">
                                 <div style="display: flex;">
-                                    <div>
-                                        <img :src="cart.image" />
+                                    <div style="margin-right: 10px;">
+                                        <img :src="cart.image" width="60px" />
                                     </div>
                                     <div>
-                                        {{ cart.name }}
-                                        <br>
+                                        <h6>
+                                            {{ cart.name }}
+                                        </h6>
                                         {{ cart.details }}
                                         <br>
                                         {{ cart.price }}
@@ -61,24 +62,21 @@ export default {
             // 테스트용 데이터로 초기화된 cart 배열
             cart: [
                 {
-                    id: 1,
                     name: 'BTS JIN PHOTOCARD',
                     details: 'PERMISSION TO DANCE ON STAGE  미니포카',
                     price: '27,000',
                     quantity: 2,
-                    image: '../../assets/img/bts-jin-photocard.jpg',
+                    image: require('../../assets/img/jin.jpg'),
                     tradeType: '일반경매',
                     progressStatus: '결제완료',
                     deliveryStatus: '배송준비중',
                     saleDate: '2024.04.28'
                 }, {
-                    id: 2,
-                    name: 'Product 2',
-                    details: 'COTTON 100%',
-                    price: 20,
+                    name: 'BTS JIN PHOTOCARD',
+                    details: 'PERMISSION TO DANCE ON STAGE  미니포카',
+                    price: '34,000',
                     quantity: 1,
-                    image: 'product2.jpg',
-                    image: '../../assets/img/bts-jin-photocard.jpg',
+                    image: require('../../assets/img/jin.jpg'),
                     tradeType: '일반경매',
                     progressStatus: '결제완료',
                     deliveryStatus: '배송준비중',
