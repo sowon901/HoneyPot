70e2264bdaf9b92ad2ec3902d76bd06a45c59fed
Leemalgum <withxia1215@naver.com>
2024-04-28 23:20:14 +0900

    feat: bid-history.vue 추가

---
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/bid-history.vue
@@ -0,0 +1,43 @@
+<template>
+     <div class="container">
+        <div class="wrapper">
+            <div class="content">
+                <div class="sidebar">
+                    <Sidebar/>
+                </div>
+                <div class="cart-items">
+                    <BidList />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import Sidebar from '../components/all-products/Sidebar'
+    import BidList from '../components/cart/BidList'
+
+    export default {
+        components: {
+            Sidebar,
+            BidList
+        }
+    }
+</script>
+<style scoped>
+.wrapper {
+    display: flex;
+    flex-direction: column;
+}
+
+.content {
+    display: flex;
+}
+
+.sidebar {
+    flex: 0 0 25%; /* 사이드바 너비 */
+}
+
+.cart-items {
+    flex: 1; /* 카트 아이템이 나머지 공간을 채움 */
+}
+</style>
