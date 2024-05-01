6f5bea189e07890e7aa092be0650420a4a82accd
Leemalgum <withxia1215@naver.com>
2024-05-01 22:02:40 +0900

    feat: products.vue 경매 판매 목록 추가

--- front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/products.vue
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/products.vue
@@ -1,47 +1,96 @@
 <template>
-  <div>
-    <TopHeader />
-    <Menubar />
-    <!-- Start Page Title Area -->
-    <div class="page-title-area">
-      <div class="container">
-        <ul>
-          <li><nuxt-link to="/">Home</nuxt-link></li>
-          <li>All Products</li>
-        </ul>
-      </div>
+  <div class="container">
+    <div class="section-title">
+      <h2>경매 목록</h2>
     </div>
-    <!-- End Page Title Area -->
-
-    <!-- Start Collections Area -->
-    <div class="products-collections-area ptb-60">
-      <div class="container">
-        <div class="section-title">
-          <h2><span class="dot"></span> Products</h2>
+    <div class="products-filter-options">
+      <div class="row align-items-center">
+        <div class="col d-flex">
+          <p> 전체 {{ totalProducts }}개 중 {{ startIndex }} - {{ endIndex }}개 상품 표시 </p>
         </div>
 
-        <div class="row">
-          <Sidebar />
-          <AllProducts />
+        <div class="col d-flex">
+          <div class="products-ordering-list">
+            <select v-model="selectedSorting" @change="sortProducts">
+              <option value="1">최신순</option>
+              <option value="2">조회순</option>
+              <option value="3">마감 임박순</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
-    <!-- End Collections Area -->
+
+    <div class="row">
+      <ProductItem v-for="(product, index) in displayedProducts" :product="product" :key="index"
+        :className="`col-lg-3 col-md-6 col-sm-6`"></ProductItem>
+    </div>
+    <nav class="woocommerce-pagination">
+      <ul>
+        <li v-for="pageNumber in totalPages" :key="pageNumber">
+          <a href="#" class="page-numbers" @click="changePage(pageNumber)">{{ pageNumber }}</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
 <script>
-import TopHeader from '../layouts/TopHeader'
-import Menubar from '../layouts/Menubar'
-import Sidebar from '../components/all-products/Sidebar'
-import AllProducts from '../components/all-products/AllProducts'
+import ProductItem from '../components/landing-one/ProductItem'
 
 export default {
   components: {
-    TopHeader,
-    Menubar,
-    Sidebar,
-    AllProducts,
+    ProductItem,
+  },
+  computed: {
+    products() {
+      return this.$store.state.products.all;
+    },
+    totalProducts() {
+      return this.products.length;
+    },
+    startIndex() {
+      return (this.currentPage - 1) * this.itemsPerPage + 1;
+    },
+    endIndex() {
+      return Math.min(this.currentPage * this.itemsPerPage, this.totalProducts);
+    },
+    displayedProducts() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = Math.min(startIndex + this.itemsPerPage, this.totalProducts);
+      return this.products.slice(startIndex, endIndex);
+    },
+    totalPages() {
+      return Math.ceil(this.totalProducts / this.itemsPerPage);
+    },
+  },
+  data() {
+    return {
+      currentPage: 1,
+      itemsPerPage: 40,
+      selectedSorting: '1'
+    };
+  },
+  methods: {
+    changePage(pageNumber) {
+      this.currentPage = pageNumber;
+    },
+    sortProducts() {
+      switch (this.selectedSorting) {
+        case '1': // 최신순
+          this.products.sort((a, b) => new Date(b.date) - new Date(a.date));
+          break;
+        case '2': // 조회순
+          this.products.sort((a, b) => b.views - a.views);
+          break;
+        case '3': // 마감 임박순
+          this.products.sort((a, b) => new Date(a.deadline) - new Date(b.deadline));
+          break;
+        default:
+          // 기본적으로는 최신순으로 정렬합니다.
+          this.products.sort((a, b) => new Date(b.date) - new Date(a.date));
+      }
+    },
   },
 }
 </script>
