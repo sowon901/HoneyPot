500c59eb69aba43ccbb940cc4fd3a22903507690
TaeJinKim0930 <108087591+TaeJinKim0930@users.noreply.github.com>
2024-05-04 15:57:36 +0900

    Revert "Feat: 결제페이지 추가 (#17)" (#18)
    This reverts commit 7be1d37b767a74b1e426e8230f4ca448c8c27f8f.

--- front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/checkout.vue
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/checkout.vue
@@ -2,16 +2,6 @@
     <div>
         <TopHeader />
         <Menubar />
-        <!-- Start Page Title Area -->
-        <div class="page-title-area">
-            <div class="container">
-                <ul>
-                    <li><nuxt-link to="/">메인페이지</nuxt-link></li>
-                    <li>결제페이지</li>
-                </ul>
-            </div>
-        </div>
-        <CartItems />
         <CheckoutItem />
     </div>
 </template>
@@ -19,16 +9,12 @@
 <script>
 import TopHeader from '../layouts/TopHeader'
 import Menubar from '../layouts/Menubar'
-// import CartItems from '../components/cart/CartItems'
-// import CheckoutItem from '../components/cart/CheckoutItem';
-import CheckoutItem from '../components/cart/honeyCheckoutItem';
+import CheckoutItem from '../components/cart/CheckoutItem';
 export default {
     components: {
         TopHeader,
         Menubar,
-        // CartItems,
         CheckoutItem,
-        
     }
 }
 </script>
