b0c8152e4238eb16f0798ef80079077c10bb2e98
JiHyun Lee <supernova63@naver.com>
2024-05-08 11:33:53 +0900

    feat : 관리자 페이지 회원 상세 추가

---
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/components/admin/UserDetails.vue
@@ -0,0 +1,17 @@
+<template>
+    <tr>
+        <td>${{user.serialNumber}}</td>
+        <td>{{user.id}}</td>
+        <td>${{user.birthdate}}</td>
+        <td>${{user.nickname}}</td>
+        <td>${{address.roadAddress}}</td>
+        <td>${{user.registrationDate}}</td>
+        <td><span class="badge badge-primary">Received</span></td>
+    </tr>
+</template>
+
+<script>
+export default {
+    props: ['user', 'address']
+}
+</script>
