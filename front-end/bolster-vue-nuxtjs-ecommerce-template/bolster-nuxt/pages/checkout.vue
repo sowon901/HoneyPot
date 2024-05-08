4660322b07251dc1caf73258592069499b321d4a
Leemalgum <withxia1215@naver.com>
2024-05-08 15:20:11 +0900

    refactor: 결제 페이지 중복 헤더, 메뉴바 제거

--- front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/checkout.vue
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/checkout.vue
@@ -1,19 +1,13 @@
 <template>
     <div>
-        <TopHeader />
-        <Menubar />
         <CheckoutItem />
     </div>
 </template>
 
 <script>
-import TopHeader from '../layouts/TopHeader'
-import Menubar from '../layouts/Menubar'
 import CheckoutItem from '../components/cart/CheckoutItem';
 export default {
     components: {
-        TopHeader,
-        Menubar,
         CheckoutItem,
     }
 }
