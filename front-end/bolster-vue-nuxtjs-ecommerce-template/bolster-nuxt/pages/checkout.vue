f7034bbc2df4dab6e07430b019a9baa285bef48a
TaeJinKim0930 <108087591+TaeJinKim0930@users.noreply.github.com>
2024-05-08 12:18:01 +0900

    Feat: 결제페이지 추가 (#19)
    * Feat:결제 페이지 추가
    
    기존에 장바구니와 결제가 따로 있던 것을 하나의 파일로 합쳐놓았습니다.
    
    * Refactor: 기존에 있던 장바구니를 없애고 결제로 합쳤습니다
    
    honeyCheckoutItem 파일이 새로 생김에 따라 기존에 있던 checkout (결제 페이지)를 업데이트 해주었습니다

--- front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/checkout.vue
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/checkout.vue
@@ -2,6 +2,16 @@
     <div>
         <TopHeader />
         <Menubar />
+        <!-- Start Page Title Area -->
+        <div class="page-title-area">
+            <div class="container">
+                <ul>
+                    <li><nuxt-link to="/">메인페이지</nuxt-link></li>
+                    <li>결제페이지</li>
+                </ul>
+            </div>
+        </div>
+        <CartItems />
         <CheckoutItem />
     </div>
 </template>
@@ -9,12 +19,16 @@
 <script>
 import TopHeader from '../layouts/TopHeader'
 import Menubar from '../layouts/Menubar'
-import CheckoutItem from '../components/cart/CheckoutItem';
+// import CartItems from '../components/cart/CartItems'
+// import CheckoutItem from '../components/cart/CheckoutItem';
+import CheckoutItem from '../components/cart/honeyCheckoutItem';
 export default {
     components: {
         TopHeader,
         Menubar,
+        // CartItems,
         CheckoutItem,
+        
     }
 }
 </script>
