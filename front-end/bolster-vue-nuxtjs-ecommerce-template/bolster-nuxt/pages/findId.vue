a5fce6ca1557a3fb61f39bc74686ab09385d9744
JiHyun Lee <89053934+ImaginCreature@users.noreply.github.com>
2024-05-08 14:28:08 +0900

    Refactor : 카카오 로그인, 아이디/비밀번호 찾기 추가 (#25)
    * chore: 카카오 로그인 img 추가
    
    * refactor: login 관련 scss 수정
    
    .login-form {
        .form-group {
            margin-bottom: 20px;
    
    30->20px 로 수정
    
    * refactor: 카카오로그인 UI 추가
    
    * refactor : 카카오 로그인 연동
    
    * feat : 아이디 찾기 추가
    
    * feat : 비밀번호 찾기 페이지 추가
    
    * refactor : index.html 에 우편번호 찾기 api, 카카오 로그인 api 추가

---
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/findId.vue
@@ -0,0 +1,131 @@
+<template>
+    <div>
+        <div class="page-title-area">
+            <div class="container">
+                <ul>
+                    <li>
+                        <nuxt-link to="/">Home</nuxt-link>
+                    </li>
+                    <li>Login</li>
+                    <li>아이디 찾기</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="login-area ptb-60">
+            <div class="container">
+                <div class="row">
+
+                    <div class="login-content">
+                        <div class="section-title" style="background-color: white;">
+                            <h2>아이디 찾기</h2>
+                        </div>
+
+                        <form
+                            class="login-form"
+                            v-if="!$store.state.authUser"
+                            @submit.prevent="login"
+                        >
+                            <p v-if="formError" class="error">
+                                {{ formError }}
+                            </p>
+                            <div class="form-group">
+                                <label>이름</label>
+                                <input
+                                    v-model="name"
+                                    type="text"
+                                    class="form-control"
+                                    placeholder=""
+                                    name="name"
+                                />
+                            </div>
+
+                            <div class="form-group">
+                                <label>휴대폰 번호</label>
+                                <input
+                                    v-model="mobileNumber"
+                                    type="text"
+                                    class="form-control"
+                                    placeholder=""
+                                    name="mobileNumber"
+                                />
+                            </div>
+
+                            <div class="form-group">
+                                <label>인증번호</label>
+                                <input
+                                    v-model="certification"
+                                    type="text"
+                                    class="form-control"
+                                    placeholder=""
+                                    name="certification"
+                                />
+                            </div>
+
+                            <div class="form-group">
+                                <label>email</label>
+                                <input
+                                    v-model="email"
+                                    type="email"
+                                    class="form-control"
+                                    placeholder=""
+                                    name="email"
+                                />
+                            </div>
+
+                            <button id="findId-btn" class="btn btn-primary">아이디 찾기</button>
+
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.links-container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.forgot-password, .forgot-id, .honeypot-signup, .separator {
+    display: inline-block;
+}
+
+.btn-primary {
+    background-color: #ffb400;
+    border: none;
+
+    &:hover {
+        border: 1px solid black;
+    }
+}
+
+</style>
+
+<script>
+import TopHeader from '../layouts/TopHeader'
+import Menubar from '../layouts/Menubar'
+
+export default {
+    components: {
+        TopHeader,
+        Menubar,
+    },
+    mounted() {
+    },
+    data() {
+        return {
+            formError: null,
+            formUsername: '',
+            formPassword: '',
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
