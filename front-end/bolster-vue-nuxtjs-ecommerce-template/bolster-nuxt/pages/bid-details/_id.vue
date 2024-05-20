9e853d7236c5f3a88ad8cb1ee18c4990ead0dd8a
Leemalgum <withxia1215@naver.com>
2024-05-21 00:05:19 +0900

    chore: package.json

--- front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/bid-details/_id.vue
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/bid-details/_id.vue
@@ -328,6 +328,22 @@ export default {
             console.log(this.price,"가격")       
          },
 
+         bidConfirm() {
+            const bidData = {
+                productId: this.product.id,
+                bidAmount: this.totalAmount,
+            };
+            axios.post('http://localhost:8080/bid', bidData)
+                .then(response => {
+                    console.log('입찰 성공:', response.data);
+                    this.bidCheck = false;
+                    window.location.reload();
+                })
+                .catch(error => {
+                    console.error('입찰 실패:', error);
+                });
+        },
+
         // remainingTime() {
         //     const currentTime = new Date();
         //     const timeLimit = new Date(this.product.timeLimit);
