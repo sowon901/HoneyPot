657c88d61418b7da307fafb81a80fc75f0489bf6
JiHyun Lee <supernova63@naver.com>
2024-05-25 17:09:38 +0900

    feat : 아이디찾기 / 찾은 아이디 표시 페이지들 추가

--- front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/findId.vue
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/findId.vue
@@ -14,71 +14,72 @@
 
         <div class="login-area ptb-60">
             <div class="container">
-                <div class="row">
 
-                    <div class="login-content">
-                        <div class="section-title" style="background-color: white;">
-                            <h2>아이디 찾기</h2>
-                        </div>
 
-                        <form
-                            class="login-form"
-                            v-if="!$store.state.authUser"
-                            @submit.prevent="login"
-                        >
-                            <p v-if="formError" class="error">
-                                {{ formError }}
-                            </p>
-                            <div class="form-group">
-                                <label>이름</label>
-                                <input
-                                    v-model="name"
-                                    type="text"
-                                    class="form-control"
-                                    placeholder=""
-                                    name="name"
-                                />
-                            </div>
-
-                            <div class="form-group">
-                                <label>휴대폰 번호</label>
-                                <input
-                                    v-model="mobileNumber"
-                                    type="text"
-                                    class="form-control"
-                                    placeholder=""
-                                    name="mobileNumber"
-                                />
-                            </div>
-
-                            <div class="form-group">
-                                <label>인증번호</label>
-                                <input
-                                    v-model="certification"
-                                    type="text"
-                                    class="form-control"
-                                    placeholder=""
-                                    name="certification"
-                                />
-                            </div>
-
-                            <div class="form-group">
-                                <label>email</label>
-                                <input
-                                    v-model="email"
-                                    type="email"
-                                    class="form-control"
-                                    placeholder=""
-                                    name="email"
-                                />
-                            </div>
-
-                            <button id="findId-btn" class="btn btn-primary">아이디 찾기</button>
-
-                        </form>
+                <div class="login-content">
+                    <div class="section-title" style="background-color: white;">
+                        <h2>아이디 찾기</h2>
                     </div>
 
+                    <form
+                        class="login-form"
+                        v-if="!$store.state.authUser"
+                     >
+                        <p v-if="formError" class="error">
+                            {{ formError }}
+                        </p>
+                        <div class="form-group">
+                            <label>이름</label>
+                            <input
+                                v-model="name"
+                                type="text"
+                                class="form-control"
+                                placeholder=""
+                                name="name"
+                            />
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-8">
+                                <div class="form-group">
+                                    <label>휴대폰 번호</label>
+                                    <input
+                                        v-model="mobileNumber"
+                                        type="text"
+                                        class="form-control"
+                                        placeholder=""
+                                        name="mobileNumber"
+                                    />
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label style="visibility: hidden;">미사용 라벨</label>
+                                    <button id="findId-btn" class="btn btn-primary" @click="sendMMS">인증번호 받기</button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label>인증번호</label>
+                            <input
+                                v-model="authCodeInput"
+                                type="text"
+                                class="form-control"
+                                placeholder=""
+                                name="authCode"
+                            />
+                            <p v-if="authCodeMessage" :style="{ color: authCodeValid ? 'green' : 'red' }">
+                                {{ authCodeMessage }}
+                            </p>
+                        </div>
+
+
+                        <button id="findId-btn" class="btn btn-primary" :disabled="!authCodeValid" @click="findId">아이디 찾기</button>
+
+                    </form>
                 </div>
+
             </div>
         </div>
     </div>
@@ -107,25 +108,97 @@
 </style>
 
 <script>
-import TopHeader from '../layouts/TopHeader'
-import Menubar from '../layouts/Menubar'
+import { ref, reactive, onMounted, watch } from 'vue';
+import axios from 'axios';
+import TopHeader from '../layouts/TopHeader';
+import Menubar from '../layouts/Menubar';
 
 export default {
+    name: 'findIdPage',
     components: {
         TopHeader,
         Menubar,
     },
-    mounted() {
-    },
-    data() {
-        return {
-            formError: null,
-            formUsername: '',
-            formPassword: '',
-        }
-    },
-    methods: {
+    setup() {
+        const formError = ref(null);
+        const name = ref('');
+        const mobileNumber = ref('');
+        const authCode = ref('');
+        const authCodeInput = ref('');
+        const authCodeValid = ref(false);
+        const authCodeMessage = ref('');
+        const userId = ref('');
 
-    },
-}
+        const sendMMS = async () => {
+            if (!name.value || !mobileNumber.value) {
+                formError.value = '이름과 휴대폰 번호를 입력해 주세요.';
+                return;
+            }
+
+            formError.value = null;
+
+            try {
+                const response = await axios.post('http://localhost:8080/mms/send-one', null, {
+                    params: {
+                        name: name.value,
+                        mobileNumber: mobileNumber.value,
+                    },
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                });
+
+                if (response.status === 200 && response.data.success) {
+                    alert('인증번호가 발송되었습니다.');
+                    authCode.value = response.data.data.authCode;
+                    userId.value = response.data.data.userId;
+                } else if (response.status === 404) {
+                    alert('해당되는 사용자가 없습니다.');
+                } else {
+                    alert('서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.');
+                }
+            } catch (error) {
+                alert('서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.');
+                console.error('Unexpected error:', error);
+            }
+        };
+
+        const findId = () => {
+            if (authCodeValid.value) {
+                this.$router.push({
+                    path: '/foundId',
+                    query: { userId: userId.value },
+                });
+            }
+        };
+
+        watch(authCodeInput, (val) => {
+            if (val === authCode.value) {
+                authCodeValid.value = true;
+                authCodeMessage.value = '인증번호가 일치합니다.';
+            } else {
+                authCodeValid.value = false;
+                authCodeMessage.value = '인증번호가 다릅니다.';
+            }
+        });
+
+        onMounted(() => {
+            // Any code that needs to run when the component is mounted
+        });
+
+        return {
+            formError,
+            name,
+            mobileNumber,
+            authCode,
+            authCodeInput,
+            authCodeValid,
+            authCodeMessage,
+            userId,
+            sendMMS,
+            findId
+        };
+    }
+};
+
 </script>
---
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/foundId.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+        <div class="page-title-area">
+            <div class="container">
+                <ul>
+                    <li>
+                        <nuxt-link to="/">Home</nuxt-link>
+                    </li>
+                    <li>Login</li>
+                    <li>아이디 찾기</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="login-area ptb-60">
+            <div class="container">
+                <div class="login-content">
+                    <div class="result-box" style="background-color: #ffd97f; padding: 20px; border-radius: 5px; text-align: center;">
+                        <h2>고객님의 아이디는 {{ userId }} 입니다</h2>
+                    </div>
+                    <div style="text-align: center; margin-top: 20px;">
+                        <nuxt-link to="/" class="btn btn-primary">메인으로 돌아가기</nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.links-container {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.forgot-password, .forgot-id, .honeypot-signup, .separator {
+    display: inline-block;
+}
+
+.btn-primary {
+    background-color: #ffb400;
+    border: none;
+    color: black;
+    padding: 10px 20px;
+    text-decoration: none;
+    border-radius: 5px;
+}
+
+.btn-primary:hover {
+    border: 1px solid black;
+    background-color: #ffc107;
+}
+</style>
+
+<script>
+import TopHeader from '../layouts/TopHeader';
+import Menubar from '../layouts/Menubar';
+
+export default {
+    components: {
+        TopHeader,
+        Menubar,
+    },
+    mounted() {
+        this.userId = this.$route.query.userId;
+    },
+    data() {
+        return {
+            userId: '',
+        }
+    },
+    methods: {
+    },
+}
+</script>
