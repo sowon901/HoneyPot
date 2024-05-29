42ad999ba9b4ab563f098191b04fd31bedeb02bd
JiHyun Lee <supernova63@naver.com>
2024-05-30 07:28:00 +0900

    feat : 비밀번호 찾기, 비밀번호 재설정 페이지 등록

--- front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/findPassword.vue
+++ front-end/bolster-vue-nuxtjs-ecommerce-template/bolster-nuxt/pages/findPassword.vue
@@ -15,16 +15,16 @@
         <div class="login-area ptb-60">
             <div class="container">
                 <div class="row">
-
                     <div class="login-content">
                         <div class="section-title" style="background-color: white;">
                             <h2>비밀번호 찾기</h2>
+                            <h5>메일로 비밀번호 재설정 링크를 보내드립니다.</h5>
                         </div>
 
                         <form
                             class="login-form"
                             v-if="!$store.state.authUser"
-                            @submit.prevent="login"
+                            @submit.prevent="findPassword"
                         >
                             <p v-if="formError" class="error">
                                 {{ formError }}
@@ -42,18 +42,18 @@
                             </div>
 
                             <div class="form-group">
-                                <label>이름</label>
+                                <label>전화번호</label>
                                 <input
-                                    v-model="name"
+                                    v-model="mobileNumber"
                                     type="text"
                                     class="form-control"
                                     placeholder=""
-                                    name="name"
+                                    name="mobileNumber"
                                 />
                             </div>
 
                             <div class="form-group">
-                                <label>email</label>
+                                <label>이메일</label>
                                 <input
                                     v-model="email"
                                     type="email"
@@ -63,11 +63,9 @@
                                 />
                             </div>
 
-                            <button id="findId-btn" class="btn btn-primary">아이디 찾기</button>
-
+                            <button id="findId-btn" class="btn btn-primary">비밀번호 찾기</button>
                         </form>
                     </div>
-
                 </div>
             </div>
         </div>
@@ -93,29 +91,52 @@
         border: 1px solid black;
     }
 }
-
 </style>
 
 <script>
-import TopHeader from '../layouts/TopHeader'
-import Menubar from '../layouts/Menubar'
+import axios from 'axios';
+import TopHeader from '../layouts/TopHeader';
+import Menubar from '../layouts/Menubar';
 
 export default {
+    name: 'ForgotPasswordPage',
     components: {
         TopHeader,
         Menubar,
     },
-    mounted() {
-    },
     data() {
         return {
             formError: null,
-            formUsername: '',
-            formPassword: '',
-        }
+            userId: '',
+            mobileNumber: '',
+            email: '',
+        };
     },
     methods: {
+        async findPassword() {
+            if (!this.userId || !this.mobileNumber || !this.email) {
+                this.formError = "모든 필드를 입력해 주세요.";
+                return;
+            }
 
+            try {
+                const response = await axios.post('http://localhost:8080/forgot-password', {
+                    userId: this.userId,
+                    mobileNumber: this.mobileNumber,
+                    email: this.email,
+                });
+
+                if (response.status === 200) {
+                    alert("비밀번호 재설정 링크가 이메일로 발송되었습니다.");
+                    this.$router.push('/');
+                } else {
+                    this.formError = "사용자를 찾을 수 없습니다.";
+                }
+            } catch (error) {
+                console.error("Error finding password:", error);
+                this.formError = "비밀번호 찾기 중 오류가 발생했습니다. 다시 시도해 주세요.";
+            }
+        },
     },
-}
+};
 </script>
